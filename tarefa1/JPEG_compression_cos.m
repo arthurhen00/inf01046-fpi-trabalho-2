d099b6c0dec29e74771ffba22d09be5a3a8196e5
Arthur Hendges <50963897+arthurhen00@users.noreply.github.com>
2023-08-23 07:57:57 -0300

    tarefa1

---
+++ tarefa1/JPEG_compression_cos.m
@@ -0,0 +1,136 @@
+function saida = JPEG_compression_cos(gray_image, compressao)
+
+    % Parte básica do JPEG (DCT e quantização)
+    % Divisão da imagem em blocos, 8x8 ou 16x16
+    % Transformada de cosseno discreta DCT
+    % Quantizacao
+    % 
+
+    clc;
+    close all;
+
+    % Imagens
+    imagem = gray_image;
+    [altura, largura] = size(imagem);
+
+    % Tamanho do bloco
+    tam_bloco = 8;
+
+    % Calcular as novas dimensões para serem múltiplas de tamanho_bloco
+    nova_altura = ceil(altura / tam_bloco) * tam_bloco;
+    nova_largura = ceil(largura / tam_bloco) * tam_bloco;
+
+    imagem = imresize(imagem, [nova_altura, nova_largura]);
+
+    % Matriz para guardar a DCT
+    DCT = zeros(altura, largura);
+    
+    % ******************************************************************
+    % COMP
+    % Aplicar a DCT em blocos 8x8
+    for i = 1:nova_altura/tam_bloco
+        for j = 1:nova_largura/tam_bloco
+            y_inicio = (i - 1) * tam_bloco + 1;
+            y_fim = y_inicio + tam_bloco - 1;
+            y_fim = min(y_fim, nova_altura); % Limitar y_fim às dimensões da imagem
+            
+            x_inicio = (j - 1) * tam_bloco + 1;
+            x_fim = x_inicio + tam_bloco - 1;
+            x_fim = min(x_fim, nova_largura); % Limitar x_fim às dimensões da imagem
+            
+            bloco = double(imagem(y_inicio:y_fim, x_inicio:x_fim));
+            dct_bloco = dct2(bloco);
+            DCT(y_inicio:y_fim, x_inicio:x_fim) = dct_bloco;
+        end
+    end
+    
+    % ******************************************************************
+    %
+    % Matriz de quantização
+    matriz_quantizacao = [
+                    16 11 10 16  24  40  51  61;
+                    12 12 14 19  26  58  60  55;
+                    14 13 16 24  40  57  69  56;
+                    14 17 22 29  51  87  80  62;
+                    18 22 37 56  68 109 103  77;
+                    24 35 55 64  81 104 113  92;
+                    49 64 78 87 103 121 120 101;
+                    72 92 95 98 112 100 103  99;];
+
+    matriz_quantizacao = matriz_quantizacao * compressao;
+
+    % Inicializar a matriz para armazenar os coeficientes quantizados
+    coeficientes_quantizados = zeros(nova_altura, nova_largura);
+
+    % Aplicar a quantização aos coeficientes DCT
+    for i = 1:nova_altura/tam_bloco
+        for j = 1:nova_largura/tam_bloco
+            y_inicio = (i - 1) * tam_bloco + 1;
+            y_fim = y_inicio + tam_bloco - 1;
+            
+            x_inicio = (j - 1) * tam_bloco + 1;
+            x_fim = x_inicio + tam_bloco - 1;
+            
+            dct_bloco = DCT(y_inicio:y_fim, x_inicio:x_fim);
+            quantizacao_bloco = round(dct_bloco ./ matriz_quantizacao); % Aplicar quantização
+            
+            coeficientes_quantizados(y_inicio:y_fim, x_inicio:x_fim) = quantizacao_bloco;
+        end
+    end
+
+    % ******************************************************************
+    % DEC
+    % Inicializar a matriz para armazenar os coeficientes inversos da DCT
+    coeficientes_idct = zeros(nova_altura, nova_largura);
+    
+    % Aplicar a inversa da DCT e inversa da quantização
+    for i = 1:nova_altura/tam_bloco
+        for j = 1:nova_largura/tam_bloco
+            y_inicio = (i - 1) * tam_bloco + 1;
+            y_fim = y_inicio + tam_bloco - 1;
+            
+            x_inicio = (j - 1) * tam_bloco + 1;
+            x_fim = x_inicio + tam_bloco - 1;
+            
+            quantizacao_bloco = coeficientes_quantizados(y_inicio:y_fim, x_inicio:x_fim);
+            idct_bloco = quantizacao_bloco .* matriz_quantizacao; % Inversa da quantização
+            
+            idct_bloco = idct2(idct_bloco); % Inversa da DCT
+            
+            coeficientes_idct(y_inicio:y_fim, x_inicio:x_fim) = idct_bloco;
+        end
+    end
+
+    % ******************************************************************
+    %
+    % Inicializar a matriz para a imagem reconstruída
+    %
+    %imagem_reconstruida = zeros(nova_altura, nova_largura);
+    %
+    % Reconstruir a imagem completa unindo os blocos reconstruídos
+    %for i = 1:nova_altura/tam_bloco
+    %    for j = 1:nova_largura/tam_bloco
+    %        y_inicio = (i - 1) * tam_bloco + 1;
+    %        y_fim = y_inicio + tam_bloco - 1;
+    %        
+    %        x_inicio = (j - 1) * tam_bloco + 1;
+    %        x_fim = x_inicio + tam_bloco - 1;
+    %        
+    %        bloco_reconstruido = coeficientes_idct(y_inicio:y_fim, x_inicio:x_fim);
+    %        imagem_reconstruida(y_inicio:y_fim, x_inicio:x_fim) = bloco_reconstruido;
+    %    end
+    %end
+    %
+    % Exibe
+    %subplot(2,2,1), imshow(uint8(imagem)), title('original');
+    %subplot(2,2,3), imshow(uint8(DCT)), title('DCT');
+    %subplot(2,2,4), imshow(uint8(coeficientes_quantizados)), title('coeficientes_quantizados');
+    %subplot(2,2,2), imshow(uint8(coeficientes_idct)), title('imagem_recontruida');
+
+    imwrite(uint8(coeficientes_idct), sprintf('./saida/saida_%d.jpg', compressao), 'jpg');
+
+    parte_selecionada = coeficientes_idct(1:altura, 1:largura);
+
+    saida = parte_selecionada;
+
+end
---
+++ tarefa1/tarefa1.m
@@ -0,0 +1,29 @@
+
+% Imagem 1
+imagem1 = imread('pingu.jpg');
+imagem1 = rgb2gray(imagem1);
+
+saida1_1 = JPEG_compression_cos(imagem1, 3);
+saida1_2 = JPEG_compression_cos(imagem1, 6);
+saida1_3 = JPEG_compression_cos(imagem1, 10);
+
+[SNR1, PSNR1] = snr_psnr(double(imagem1), saida1_1);
+[SNR2, PSNR2] = snr_psnr(double(imagem1), saida1_2);
+[SNR3, PSNR3] = snr_psnr(double(imagem1), saida1_3);
+
+subplot(2,2,1), imshow(uint8(imagem1 )), title(sprintf('original'));
+subplot(2,2,2), imshow(uint8(saida1_1)), title(sprintf('comp 3 SNR:%.2f PSNR:%.2f',  SNR1, PSNR1));
+subplot(2,2,3), imshow(uint8(saida1_2)), title(sprintf('comp 5 SNR:%.2f PSNR:%.2f',  SNR2, PSNR2));
+subplot(2,2,4), imshow(uint8(saida1_3)), title(sprintf('comp 10 SNR:%.2f PSNR:%.2f', SNR3, PSNR3));
+
+% Imagem 2
+%imagem2 = imread('office_5.jpg');
+%imagem2 = rgb2gray(imagem2);
+
+%saida2_1 = JPEG_compression_cos(imagem2, 3);
+%saida2_2 = JPEG_compression_cos(imagem2, 6);
+%saida2_3 = JPEG_compression_cos(imagem2, 10);
+
+%[SNR1, PSNR1] = snr_psnr(double(imagem2), saida2_1);
+%[SNR2, PSNR2] = snr_psnr(double(imagem2), saida2_2);
+%[SNR3, PSNR3] = snr_psnr(double(imagem2), saida2_3);
