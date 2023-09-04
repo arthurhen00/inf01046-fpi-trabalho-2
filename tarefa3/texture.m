db4c7ae46ed81e1427add32ea93b743254bbb03c
Mino <dev@mino.one>
2023-09-03 21:04:21 -0300

    Tarefa 3 - Andei mas travei denovo

--- tarefa3/texture.m
+++ tarefa3/texture.m
@@ -1,22 +1,54 @@
-function [] = texture( I, filter_size, theta, nTries )
+function [] = texture( I, filter_size, theta, nTries, nGroups )
+
+close all;
 
 I_gray = double(I);
-I_size = size(I_gray);
-degree = 0:theta:360;
+numRows = size(I_gray, 1) - filter_size;
+numCols = size(I_gray, 2) - filter_size;
+degree = 0:theta:360 - theta;
 
+% Filtragem usando os filtros de gabor
 for k = 1:length(degree)
     % Pega o filtro de gabor
     filter = gb(filter_size, 8, degree(k), 4);
     
     % Convolução com a imagem original
-    for j = 1:I_size(2) - filter_size
-        for i = 1:I_size(1) - filter_size
+    for j = 1:numCols
+        for i = 1:numRows
             filtered(i,j) = mean(mean(filter .* I_gray(i:i+filter_size-1, j:j+filter_size-1)));
         end
     end
 
-    groups{k} = filtered;
+    groups(:,:,k) = filtered;
 end
 
-% ?????????????
-[idx, center] = kmeans(groups{1}, 2, 'distance', 'sqEuclidean', 'Replicates', nTries);
+% Adiciona informação espacial
+X = 1:numCols;
+Y = 1:numRows;
+[X,Y] = meshgrid(X,Y);
+groups = cat(3, cat(3,groups,X), Y);
+
+% Reshape pro formato do kmeans
+X = reshape(groups, numRows * numCols, []);
+X = bsxfun(@minus,   X, mean(X));
+X = bsxfun(@rdivide, X,  std(X));
+
+coeff = pca(X);
+feature2DImage = reshape(X*coeff(:,1), numRows, numCols);
+figure, imshow(feature2DImage,[]), title('Features');
+
+% Faz o kmeans
+[idx, center] = kmeans(X, nGroups, 'distance', 'sqEuclidean', 'Replicates', nTries);
+pixel_labels = reshape(idx, numRows, numCols);
+
+% Rotula os pixels usando os resultados do k-means
+figure, imshow(pixel_labels, []), title('Imagem rotulada');
+
+% Cria uma imagem para cada cor da imagem de entrada
+rgb_label = repmat(pixel_labels, [1 1 3]);
+
+for k = 1:nGroups
+    color = I(1:numRows, 1:numCols, :);
+    color(rgb_label ~= k) = 0;
+    figure, imshow(color), title(sprintf('Grupo %d', k));
+end
