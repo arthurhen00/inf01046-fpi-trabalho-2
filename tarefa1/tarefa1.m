0d779775e3a9269ade32478b0eee1fe4fb33c5e7
Arthur Hendges <50963897+arthurhen00@users.noreply.github.com>
2023-09-05 17:25:14 -0300

    att images

--- tarefa1/tarefa1.m
+++ tarefa1/tarefa1.m
@@ -1,6 +1,7 @@
 
+% Arquivo de chamada
 % Imagem 1
-imagem1 = imread('pingu.jpg');
+imagem1 = imread('umbrela.jpg');
 imagem1 = rgb2gray(imagem1);
 
 saida1_1 = JPEG_compression_cos(imagem1, 3);
@@ -15,6 +16,7 @@ subplot(2,2,1), imshow(uint8(imagem1 )), title(sprintf('original'));
 subplot(2,2,2), imshow(uint8(saida1_1)), title(sprintf('comp 3 SNR:%.2f PSNR:%.2f',  SNR1, PSNR1));
 subplot(2,2,3), imshow(uint8(saida1_2)), title(sprintf('comp 5 SNR:%.2f PSNR:%.2f',  SNR2, PSNR2));
 subplot(2,2,4), imshow(uint8(saida1_3)), title(sprintf('comp 10 SNR:%.2f PSNR:%.2f', SNR3, PSNR3));
+imwrite(uint8(imagem1), sprintf('./saida/entrada_original.jpg', 'jpg'));
 
 % Imagem 2
 %imagem2 = imread('office_5.jpg');
